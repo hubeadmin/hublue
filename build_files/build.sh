f9688a7d9662a1954ef7ea3f2951998956fc839f
hubeadmin <hubertstefanski@gmail.com>
2025-05-24 18:59:25 +0200

    chore: drop unnecessary installs from build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,49 +19,10 @@ dnf5 install -y \
   stack \
   xclip \
   dnf-plugins-core \
+  awscli2 \
   zellij || { echo "❌ Failed to install DNF packages"; exit 1; }
 
-echo "📦 Add Flathub Flatpak repo..."
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-### Flatpak installation
-echo "📦 Installing Flatpak apps..."
-flatpak install -y flathub \
-  app.zen_browser.zen \
-  com.bitwarden.desktop \
-  com.discordapp.Discord \
-  com.github.tchx84.Flatseal \
-  com.obsproject.Studio \
-  com.rustdesk.RustDesk \
-  com.slack.Slack \
-  com.spotify.Client \
-  com.valvesoftware.Steam \
-  io.github.dvlv.boxbuddyrs \
-  io.github.flattool.Warehouse \
-  io.missioncenter.MissionCenter \
-  md.obsidian.Obsidian \
-  org.chromium.Chromium \
-  org.gnome.Calculator \
-  org.gnome.Characters \
-  org.gnome.Logs \
-  org.gnome.NautilusPreviewer \
-  org.gnome.Weather \
-  org.gnome.World.PikaBackup \
-  org.gnome.clocks \
-  org.gnome.font-viewer \
-  org.libreoffice.LibreOffice \
-  org.localsend.localsend_app \
-  org.videolan.VLC
-
-### Install AWS CLI v2
-echo "☁️ Installing AWS CLI v2..."
-curl -sSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" || { echo "❌ Failed to download AWS CLI"; exit 1; }
-
-unzip -q awscliv2.zip || { echo "❌ Failed to unzip AWS CLI"; exit 1; }
-
-./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update || { echo "❌ Failed to install AWS CLI"; exit 1; }
-
-rm -rf awscliv2.zip aws || { echo "⚠️ Warning: Cleanup of AWS CLI temp files failed"; }
 
 ### Enable a systemd socket
 echo "🔌 Enabling podman.socket"
